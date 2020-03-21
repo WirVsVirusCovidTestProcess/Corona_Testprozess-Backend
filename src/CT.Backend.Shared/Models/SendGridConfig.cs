f6822b090ead4de911e4ca1c4c88a509d96f6b03
Alexander Beck <alexander.beck@microsoft.com>
2020-03-21 18:38:25 +0100

    Added SendGrid Config and Configbuilder

---
+++ src/CT.Backend.Shared/Models/SendGridConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Backend.Shared.Models
+{
+    public class SendGridConfig
+    {
+        public string sendGridApiUser { get; set; }
+
+        public string sendGridApiKey { get; set; }
+    }
+}

9b4f9f2f45dc4ad0ab90b1f28d407fe80eb18256
paule96 <paul-jeschke@outlook.com>
2020-03-21 11:34:44 +0100

    remove useless code

--- src/CT.Backend.Functions/CalculateScore.cs
+++ src/CT.Backend.Functions/CalculateScore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CT.Backend.Shared;
 using CT.Backend.Shared.ScoreCalculators;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
@@ -36,43 +37,8 @@ namespace CT.Backend.Functions
             log.LogInformation($"Start calculation of riskscore of {data.Token}");
             data.RiskScore = new ChariteCalculator().Calculate(data.Answers);
             log.LogInformation($"Update riskscore of {data.Token} to {data.RiskScore}");
-            await outputTable.UpsertDocumentAsync(collectionUri, data, disableAutomaticIdGeneration: true);
-
-            //var operation = TableOperation.Retrieve<QuestionData>(myQueueItem.Key, myQueueItem.Value);
-            //var result = await outputTable.ExecuteAsync(operation);
-            ////dynamic lol = result.Result;
-            ////var anwers = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Dictionary<string, string>>>(lol.Answers);
-            //var data = (result.Result as QuestionData);
-            
-            ////data.RiskScore = new ChariteCalculator().Calculate(data.Answers);
-            //data.RiskScore = 20; // TODO: this deletes the answers...
-            //log.LogInformation($"Update riskscore of {data.RowKey} to {data.RiskScore}");
-            //var mergeOperation = TableOperation.Replace(data);
-            //await outputTable.ExecuteAsync(mergeOperation);            
+            await outputTable.ReplaceDocumentAsync(UriFactory.CreateDocumentUri("QuestionsData", "QuestionsData", data.Id.ToString()), data );
+      
         }
-
-        //private class QuestionRetriveData: TableEntity
-        //{
-        //    /// <summary>
-        //    /// The answered questions
-        //    /// </summary>
-        //    public string Answers { get; set; }
-        //    /// <summary>
-        //    /// How high is the risk.
-        //    /// </summary>
-        //    public int? RiskScore { get; set; }
-
-        //    public QuestionData ConvertToData()
-        //    {
-        //        return new QuestionData(RowKey)
-        //        {
-        //            PartitionKey = PartitionKey,
-        //            Timestamp = Timestamp,
-        //            ETag = ETag,
-        //            RiskScore = RiskScore,
-        //            Answers = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Dictionary<string, string>>>(Answers)
-        //        };
-        //    }
-        //}
     }
 }

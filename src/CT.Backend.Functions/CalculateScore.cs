96fb24de6f4075559a9aa06a4869e033f44d1183
paule96 <paul-jeschke@outlook.com>
2020-03-21 04:52:13 +0100

    go back to the hard score.
    so all the other people can use the backend without exceptions

--- src/CT.Backend.Functions/CalculateScore.cs
+++ src/CT.Backend.Functions/CalculateScore.cs
@@ -19,13 +19,14 @@ namespace CT.Backend.Functions
             [Table("QuestionsData")] CloudTable outputTable,
             ILogger log)
         {
-            var operation = TableOperation.Retrieve<QuestionRetriveData>(myQueueItem.Key, myQueueItem.Value);
+            var operation = TableOperation.Retrieve<QuestionData>(myQueueItem.Key, myQueueItem.Value);
             var result = await outputTable.ExecuteAsync(operation);
-            dynamic lol = result.Result;
-            var anwers = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Dictionary<string, string>>>(lol.Answers);
-            var data = (result.Result as QuestionRetriveData).ConvertToData();
+            //dynamic lol = result.Result;
+            //var anwers = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Dictionary<string, string>>>(lol.Answers);
+            var data = (result.Result as QuestionData);
             log.LogInformation($"Start calculation of riskscore of {data.RowKey}");
-            data.RiskScore = new ChariteCalculator().Calculate(data.Answers);
+            //data.RiskScore = new ChariteCalculator().Calculate(data.Answers);
+            data.RiskScore = 20; // TODO: this deletes the answers...
             log.LogInformation($"Update riskscore of {data.RowKey} to {data.RiskScore}");
             var mergeOperation = TableOperation.Replace(data);
             await outputTable.ExecuteAsync(mergeOperation);            

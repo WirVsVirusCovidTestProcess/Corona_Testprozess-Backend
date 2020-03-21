2ffcba54329225998efed4f0153cc885653bee1a
paule96 <paul-jeschke@outlook.com>
2020-03-21 15:14:07 +0100

    add user information data

---
+++ src/CT.Backend.Shared/ViewModel/UserInformationView.cs
@@ -0,0 +1,28 @@
+﻿using CT.Backend.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Backend.Shared.ViewModel
+{
+    public class UserInformationViewModel : UserInformation
+    {
+        public UserInformationViewModel()
+        {
+
+        }
+        public UserInformationViewModel(UserInformation baseModel)
+        {
+            Email = baseModel.Email;
+            FirstName = baseModel.FirstName;
+            Id = baseModel.Id;
+            LastName = baseModel.LastName;
+            Location = baseModel.Location;
+            RiskScore = baseModel.RiskScore;
+            Source = baseModel.Source;
+            Token = baseModel.Token;
+        }
+        public string QuestionToken { get; set; }
+        public new int? RiskScore {get;}
+    }
+}
